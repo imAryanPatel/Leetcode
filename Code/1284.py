0dcba675e862ab8fb180b46f3487c36328db037b
imAryanPatel <ap028803@gmail.com>
2026-03-02 00:18:59 +0530

    1284

---
+++ Code/1284.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findSpecialInteger(self, arr):
+        n = len(arr)
+        from collections import Counter
+        freq = Counter(arr)
+
+        max_num = max(freq, key=freq.get)
+
+        return max_num
+class Solution(object):
+    def findSpecialInteger(self, arr):
+        
+        n = len(arr)
+        quarter = n // 4
+        
+        for i in range(n - quarter):
+            if arr[i] == arr[i + quarter]:
+                return arr[i]       
